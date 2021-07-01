fcec1e7bbe0d821921a5f70590704d7f1a609b89
AnthonyArmour <anthonydarmour@gmail.com>
2021-07-01 13:15:11 -0500

    added image init changes

--- models/__init__.py
+++ models/__init__.py
@@ -2,31 +2,83 @@ from models import tech_obj, person_obj, question
 
 
 """person_obj images"""
+derek_images = []
+tori_images = []
+libby_images = []
+stan_images = []
+kristen_images = []
 
-derek_img = "../assets/images/"
-tori_img = "../assets/images/"
-libby_img = "../assets/images/"
-stan_img = "../assets/images/"
-guillaume_img = "../assets/images/"
+derek_images.append("../assets/images/derek_1.PNG")
+derek_images.append("../assets/images/derek_2.PNG")
+derek_images.append("../assets/images/derek_3.PNG")
+
+tori_images.append("../assets/images/tori_1.PNG")
+tori_images.append("../assets/images/tori_2.PNG")
+tori_images.append("../assets/images/tori_3.PNG")
+
+libby_images.append("../assets/images/libby_1.PNG")
+libby_images.append("../assets/images/libby_2.PNG")
+libby_images.append("../assets/images/libby_3.PNG")
+
+stan_images.append("../assets/images/stan_1.PNG")
+stan_images.append("../assets/images/stan_2.PNG")
+stan_images.append("../assets/images/stan_3.PNG")
+
+kristen_images.append("../assets/images/kristen_1.PNG")
+kristen_images.append("../assets/images/kristen_2.PNG")
+kristen_images.append("../assets/images/kristen_3.PNG")
+
+# derek_img = "../assets/images/"
+# tori_img = "../assets/images/"
+# libby_img = "../assets/images/"
+# stan_img = "../assets/images/"
+# guillaume_img = "../assets/images/"
 
 
 """tech_obj images"""
+google_images = []
+intranet_images = []
+vs_code_images = []
+vagrant_images = []
+github_images = []
 
-google_img = "../assets/images/"
-intranet_img = "../assets/images/"
-vs_code_img = "../assets/images/"
-vagrant_img = "../assets/images/"
-github_img = "../assets/images/"
+google_images.append("../assets/images/google_1.PNG")
+google_images.append("../assets/images/google_2.PNG")
+google_images.append("../assets/images/google_3.PNG")
+
+intranet_images.append("../assets/images/intranet_1.PNG")
+intranet_images.append("../assets/images/intranet_2.PNG")
+intranet_images.append("../assets/images/intranet_3.PNG")
+
+vs_code_images.append("../assets/images/vscode_1.PNG")
+vs_code_images.append("../assets/images/vscode_2.PNG")
+vs_code_images.append("../assets/images/vscode_3.PNG")
+
+vagrant_images.append("../assets/images/")
+vagrant_images.append("../assets/images/")
+vagrant_images.append("../assets/images/")
+
+github_images.append("../assets/images/")
+github_images.append("../assets/images/")
+github_images.append("../assets/images/")
+
+
+
+# google_img = "../assets/images/"
+# intranet_img = "../assets/images/"
+# vs_code_img = "../assets/images/"
+# vagrant_img = "../assets/images/"
+# github_img = "../assets/images/"
 
 
 """question images"""
 
-derekQ_img = "../assets/images/"
-toriQ_img = "../assets/images/"
-libbyQ_img = "../assets/images/"
-googleQ_img = "../assets/images/"
-intranetQ_img = "../assets/images/"
-githubQ_img = "../assets/images/"
+derekQ_img = ["../assets/images/"]
+toriQ_img = ["../assets/images/"]
+libbyQ_img = ["../assets/images/"]
+googleQ_img = ["../assets/images/"]
+intranetQ_img = ["../assets/images/"]
+# githubQ_img = "../assets/images/"
 
 
 """init persons"""
@@ -34,11 +86,11 @@ githubQ_img = "../assets/images/"
 
 persons_list = []
 
-persons_list.append(person_obj(derek_img, (200, 600), "Derek"))
-persons_list.append(person_obj(tori_img, (600, 600), "Tori"))
-persons_list.append(person_obj(libby_img, (900, 600), "Libby"))
-persons_list.append(person_obj(stan_img, (600, 600), "Stan"))
-persons_list.append(person_obj(guillaume_img, (900, 600), "Guillaume"))
+persons_list.append(person_obj(derek_images, (200, 600), "Derek"))
+persons_list.append(person_obj(tori_images, (600, 600), "Tori"))
+persons_list.append(person_obj(libby_images, (900, 600), "Libby"))
+persons_list.append(person_obj(stan_images, (600, 600), "Stan"))
+persons_list.append(person_obj(kristen_images, (900, 600), "Kristen"))
 
 
 
@@ -47,11 +99,11 @@ persons_list.append(person_obj(guillaume_img, (900, 600), "Guillaume"))
 
 tech_obj_list = []
 
-tech_obj_list.append(tech_obj(google_img, (200, 600), "Google"))
-tech_obj_list.append(tech_obj(intranet_img, (600, 600), "Intranet"))
-tech_obj_list.append(tech_obj(vs_code_img, (900, 600), "VS Code"))
-tech_obj_list.append(tech_obj(vagrant_img, (600, 600), "Vagrant"))
-tech_obj_list.append(tech_obj(github_img, (900, 600), "Github"))
+tech_obj_list.append(tech_obj(google_images, (200, 600), "Google"))
+tech_obj_list.append(tech_obj(intranet_images, (600, 600), "Intranet"))
+tech_obj_list.append(tech_obj(vs_code_images, (900, 600), "VS Code"))
+tech_obj_list.append(tech_obj(vagrant_images, (600, 600), "Vagrant"))
+tech_obj_list.append(tech_obj(github_images, (900, 600), "Github"))
 
 
 """init questions"""
--- models/base_obj.py
+++ models/base_obj.py
@@ -11,10 +11,13 @@ class base_obj():
 
     newid = itertools.count().next
 
-    def __init__(self, image, pos):
+    def __init__(self, images, pos):
         self.id = base_obj.newid()
         self.active = False
-        self.image = pygame.transform.scale(pygame.image.load(image), DIM)
+        self.image_1 = pygame.transform.scale(pygame.image.load(images[0]), DIM)
+        if len(images) > 1:
+            self.image_2 = pygame.transform.scale(pygame.image.load(images[1]), DIM)
+            self.image_3 = pygame.transform.scale(pygame.image.load(images[2]), DIM)
         self.pos = pygame.Rect(pos[0], pos[1], DIM[0], DIM[1])
 
     # def correct_message():
