da4b7968ff7afbb6c6742a8a28610a139e63443d
kyle-gross <kyg555@outlook.com>
2021-07-02 15:58:19 -0700

    Move sound volume

--- main.py
+++ main.py
@@ -28,7 +28,9 @@ textRect.center = (600, 750)
 
 # Sounds
 DING = pygame.mixer.Sound(os.path.join("Assets/sounds", "ding.mp3"))
+DING.set_volume(0.01)
 BONG = pygame.mixer.Sound(os.path.join("Assets/sounds", "bong.mp3"))
+BONG.set_volume(0.01)
 
 # Misc
 START_BUTTON = pygame.Rect(475, 600, 300, 100)
@@ -197,11 +199,9 @@ def click_handler(event, question, obj_list):
             if question.name_id == obj.name:
                 obj.active = obj.image_3
                 question.correct = True
-                DING.set_volume(0.1)
                 DING.play()
             else:
                 obj.active = obj.image_2
-                BONG.set_volume(0.1)
                 BONG.play()
 
 
